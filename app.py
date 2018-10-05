5b921ea9335111d59f49d46458e3df36018333a1
Matthew <mrcwtwj@gmail.com>
2018-10-05 16:03:57 -0500

    IT WORKS!

--- app.py
+++ app.py
@@ -14,61 +14,20 @@ def webhook():
 	data = request.get_json()
 
 	# We don't want to reply to ourselves
-	if data['name'] != 'bot':
-		msg = 'hello world: user: {} data: {}'.format(data['name'], data['text'])
-		send_message(msg)
+	# if data['name'] != 'bot':
+	# 	msg = 'hello world: user: {} data: {}'.format(data['name'], data['text'])
+	# 	send_message(msg)
 
 	return "ok", 200
 
+if data['name'] == 'Geaux Sheaux' and data['text'] == 'my':
+	msg = 'here is your score GS...'
+	send_message(msg)
 
-# def index():
-# 	return 'OK!'
+
 
 def send_message(msg):
 	url = 'https://api.groupme.com/v3/bots/post'
 	data = {'text': msg, 'bot_id': "eca4646a2e4f736ab96eefa29e"}
 	request = Request(url, urlencode(data).encode())
 	json = urlopen(request).read().decode()
-
-
-# import os
-# import json
-
-# from urllib.parse import urlencode
-# from urllib.request import Request, urlopen
-
-# from flask import Flask, request
-
-# app = Flask(__name__)
-
-# msg = 'Whatttt is your favorite color?'
-
-# @app.route('/', methods=['POST'])
-# def send_message(msg):
-# 	url = 'https://api.groupme.com/v3/bots/post'
-# 	data = {'bot_id': 'eca4646a2e4f736ab96eefa29e', 'text': msg}
-# 	request = Request(url, urlencode(data).endcode())
-
-
-
-
-# def webhook():
-# 	data = request.get_json()
-
-# 	# We don't want to reply to ourselves
-# 	if data['name'] != 'bot':
-# 		msg = '{}, you sent {}.'.format(data['name'], data['text'])
-# 		send_message(msg)
-
-# 	return "ok", 200
-
-# def send_message(msg):
-# 	url = 'https://api.groupme.com/v3/bots/post'
-
-# 	data = {
-# 			'bot_id' : 'eca4646a2e4f736ab96eefa29e', 
-# 			'text' : msg
-# 			}
-
-# 	request = Request(url, urlencode(data).endcode())
-# 	json = urlopen(request).read().decode()
