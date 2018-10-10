11600d07b355572b6cbb1e81c1afdcc58d6f29e7
Matthew <mrcwtwj@gmail.com>
2018-10-10 10:11:06 -0500

    hello world

--- app.py
+++ app.py
@@ -14,54 +14,72 @@ import selenium.webdriver.chrome.service as service
 
 app = Flask(__name__)
 
-GOOGLE_CHROME_BIN = '/app/.apt/usr/bin/google-chrome'
-CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+
+return "hello world"
 
 
-@app.route('/', methods=['POST'])
-def webhook():
-	data = request.get_json()
 
-	# We don't want to reply to ourselves
-	# if data['name'] != 'bot':
-	# 	msg = 'hello world: user: {} data: {}'.format(data['name'], data['text'])
-	# 	send_message(msg)
 
-	if data['text'] == 'my':		
-		week = 5
-		season = 2018
-		scoreboard_url = 'http://games.espn.com/ffl/scoreboard?leagueId=133377&matchupPeriodId=%s&seasonId=%s' % (week, season)
+
+
+
+
+
+
+
+
+
+
+
+
+# GOOGLE_CHROME_BIN = '/app/.apt/usr/bin/google-chrome'
+# CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+
+
+# @app.route('/', methods=['POST'])
+# def webhook():
+# 	data = request.get_json()
+
+# 	# We don't want to reply to ourselves
+# 	# if data['name'] != 'bot':
+# 	# 	msg = 'hello world: user: {} data: {}'.format(data['name'], data['text'])
+# 	# 	send_message(msg)
+
+# 	if data['text'] == 'my':		
+# 		week = 5
+# 		season = 2018
+# 		scoreboard_url = 'http://games.espn.com/ffl/scoreboard?leagueId=133377&matchupPeriodId=%s&seasonId=%s' % (week, season)
 		
-		# service = service.Service('/app/.apt/usr/bin/chromedriver') #path to driver
-		# service.start()
-		# capabilities = {'chrome.binary': '/app/.apt/usr/bin/google-chrome'} #path to chrome
-		# driver = webdriver.Remote(service.service_url, capabilities)
-		# driver.get(scoreboard_url)
-		# html = driver.page_source
-		chrome_options = Options()
-		chrome_options.binary_location = GOOGLE_CHROME_BIN
-		chrome_options.add_argument('--disable-gpu')
-		chrome_options.add_argument('--no-sandbox')
-		driver = webdriver.Chrome(executable_path = CHROMEDRIVER_PATH, chrome_options=chrome_options)
-		driver.get(scoreboard_url)
-		html = driver.page_source
+# 		# service = service.Service('/app/.apt/usr/bin/chromedriver') #path to driver
+# 		# service.start()
+# 		# capabilities = {'chrome.binary': '/app/.apt/usr/bin/google-chrome'} #path to chrome
+# 		# driver = webdriver.Remote(service.service_url, capabilities)
+# 		# driver.get(scoreboard_url)
+# 		# html = driver.page_source
+# 		chrome_options = Options()
+# 		chrome_options.binary_location = GOOGLE_CHROME_BIN
+# 		chrome_options.add_argument('--disable-gpu')
+# 		chrome_options.add_argument('--no-sandbox')
+# 		driver = webdriver.Chrome(executable_path = CHROMEDRIVER_PATH, chrome_options=chrome_options)
+# 		driver.get(scoreboard_url)
+# 		html = driver.page_source
 
-		soup = BeautifulSoup(html, "lxml")
-		driver.quit()
+# 		soup = BeautifulSoup(html, "lxml")
+# 		driver.quit()
 
-		points = soup.select_one('#tmTotalPts_1')
+# 		points = soup.select_one('#tmTotalPts_1')
 
 
-		msg = 'here is your games score: '
+# 		msg = 'here is your games score: '
 		
-		send_message(msg, points)
+# 		send_message(msg, points)
 
 
-	return "ok", 200
+# 	return "ok", 200
 
 
-def send_message(msg, points):
-	url = 'https://api.groupme.com/v3/bots/'
-	data = {'text': '{} {}'.format(msg,points), 'bot_id': "eca4646a2e4f736ab96eefa29e"}
-	request = Request(url, urlencode(data).encode())
-	json = urlopen(request).read().decode()
+# def send_message(msg, points):
+# 	url = 'https://api.groupme.com/v3/bots/'
+# 	data = {'text': '{} {}'.format(msg,points), 'bot_id': "eca4646a2e4f736ab96eefa29e"}
+# 	request = Request(url, urlencode(data).encode())
+# 	json = urlopen(request).read().decode()
