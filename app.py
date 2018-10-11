bc05459f9a3d84c9b2ebaea1ff207ed9ca8d7ae8
Matthew <mrcwtwj@gmail.com>
2018-10-11 17:24:59 -0500

    options added

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from flask import Flask, request
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options 
 import selenium.webdriver.chrome.service as service
 
 app = Flask(__name__)
@@ -16,24 +17,24 @@ app = Flask(__name__)
 def webhook():
 	data = request.get_json()
 	if data['text'] == 'test':
-		# team = 1
-		# season = 2018
-		# week = 6
-		# url = 'http://games.espn.com/ffl/scoreboard?leagueId=133377&matchupPeriodId=%s&seasonId=%s' % (week, season)
-		# #chrome_options = Options()
-		# #chrome_options.binary_location = GOOGLE_CHROME_BIN
-		# # chrome_options.add_argument('--disable-gpu')
-		# # chrome_options.add_argument('--no-sandbox')
-		# CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
-		# GOOGLE_CHROME_BIN = '/app/.apt/usr/bin/google-chrome'
-		# driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, binary_location=GOOGLE_CHROME_BIN, headless=True)
-		# driver.get(url)
-		# html = driver.page_source
+		team = 1
+		season = 2018
+		week = 6
+		url = 'http://games.espn.com/ffl/scoreboard?leagueId=133377&matchupPeriodId=%s&seasonId=%s' % (week, season)
+		CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+		GOOGLE_CHROME_BIN = '/app/.apt/usr/bin/google-chrome'
+		chrome_options = Options()
+		chrome_options.binary_location = GOOGLE_CHROME_BIN
+		chrome_options.add_argument('--disable-gpu')
+		chrome_options.add_argument('--no-sandbox')
+		driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+		driver.get(url)
+		html = driver.page_source
 
-		# soup = BeautifulSoup(html, "lxml")
+		soup = BeautifulSoup(html, "lxml")
 
-		# points = soup.select_one('#tmTotalPts_1').text
-		points = 'this is how we do it'
+		points = soup.select_one('#tmTotalPts_1').text
+		# points = 'this is how we do it'
 
 		send_message(points)
 
