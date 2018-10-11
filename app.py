ec7f5adcaaec67b0c2adfca49ed123bd1a24985f
Matthew <mrcwtwj@gmail.com>
2018-10-11 10:19:59 -0500

    using req

--- app.py
+++ app.py
@@ -1,13 +1,46 @@
+import os
+import json
+
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+
 from flask import Flask, request
+from requests_html import HTMLSession
 
 app = Flask(__name__)
 
+@app.route('/', methods=['POST'])
+def webhook():
+	data = request.get_json()
+	if data['text'] == 'my':
+		team = 1
+		season = 2018
+		week = 6
+		url = 'http://games.espn.com/ffl/scoreboard?leagueId=133377&matchupPeriodId=%s&seasonId=%s' % (week, season)
+		session = HTMLSession()
+		r = session.get(url)
+		r.html.render()
+		ytp = '#team_ytp_%s' % (team)
+		pts = '#tmTotalPts_%s' % (team)
+		proj = '#team_liveproj_%s' % (team)
+		for tag in r.html.find(ytp):
+			players_remaining = tag.text
 
-print("hello world")
+		msg = players_remaining
+		
+		send_message(msg)
+
+
+	return "ok", 200
+
+def send_message(msg):
+	url = 'https://api.groupme.com/v3/bots/'
+	data = {'text': 'remaining: {}'.format(msg), 'bot_id': "eca4646a2e4f736ab96eefa29e"}
+	request = Request(url, urlencode(data).encode())
+	json = urlopen(request).read().decode()
+
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
 
-# urllib3==1.23
-# selenium==3.14.0
-# beautifulsoup4==4.6.1
+
