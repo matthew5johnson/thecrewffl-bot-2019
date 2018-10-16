6057dd4fa5e750bb8ab932d36b773cac6ebfeaf6
Matthew <mrcwtwj@gmail.com>
2018-10-15 20:03:12 -0500

    final version 1

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ def send_message(msg):
 	url = 'https://api.groupme.com/v3/bots/post'
 	message = {
 		'text': msg,  ##### Formatting wishlist: {:>8} . {:18} proj: {}   ... The error is here prob because it can't encode a list data type in the middle of a string. work with the types. .type print to console if you can't print the list itself.
-		'bot_id': 'eca4646a2e4f736ab96eefa29e'
+		'bot_id': 'ba284f3f9f43fb0ef944c59350' #'eca4646a2e4f736ab96eefa29e'
 		}
 	json = requests.post(url, message)
 	# sys.stdout.write('made it to send_message function. This was passed {} << '.format(msg))
