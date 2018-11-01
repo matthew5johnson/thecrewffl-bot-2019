b6be02f4100e4b30b33a8c8ab06c00016f22bde1
Matthew <mrcwtwj@gmail.com>
2018-10-31 19:37:44 -0500

    database_access fxn added to message.py

--- message.py
+++ message.py
@@ -14,6 +14,79 @@ import re
 import pymysql
 
 
+def database_access(table, command):
+	con = pymysql.connect(host='us-cdbr-iron-east-01.cleardb.net', user='bc01d34543e31a', password='02cdeb05', database='heroku_29a4da67c47b565')
+	cur = con.cursor()
+
+
+	##########  SETTINGS
+	if table == 'settings':
+		cur.execute("SELECT settings_rbvotes, settings_week FROM settings WHERE description='main';")
+		settings_tuple = cur.fetchall()
+		con.commit()
+		con.close()
+		if command == 'week':
+			week = int(settings_tuple[0][1])
+			return(week)
+		elif command == 'rb':
+			rb_votes = int(settings_tuple[0][0])
+			return(rb_votes)
+	
+	#########   RECORDS		
+	elif table == 'records':
+		if command == 'all':
+			# Highest single game score
+			cur.execute("SELECT weekly_points, franchise, season, week, opponent FROM weekly_records WHERE type=1 ORDER BY weekly_points desc LIMIT 1;")
+			weekly_highscore_tuple = cur.fetchall()
+			con.commit()
+			# Lowest single game score
+			cur.execute("SELECT weekly_points, franchise, season, week, opponent FROM weekly_records WHERE type=2 ORDER BY weekly_points asc LIMIT 1;")
+			weekly_lowcore_tuple = cur.fetchall()
+			con.commit()
+			# Biggest blowout
+			cur.execute("SELECT weekly_margin, franchise, season, week, opponent FROM weekly_records WHERE type=3 ORDER BY weekly_margin desc LIMIT 1;")
+			weekly_blowout_tuple = cur.fetchall()
+			con.commit()
+			# Closest game
+			cur.execute("SELECT weekly_margin, franchise, season, week, opponent FROM weekly_records WHERE type=4 ORDER BY weekly_margin asc LIMIT 1;")
+			weekly_closest_tuple = cur.fetchall()
+			con.commit()
+			# Most PPG Season
+			cur.execute("SELECT season_ppg, franchise, season FROM season_records WHERE type=5 ORDER BY season_ppg desc LIMIT 1;")
+			season_highppg_tuple = cur.fetchall()
+			con.commit()
+			# Fewest PPG Season
+			cur.execute("SELECT season_ppg, franchise, season FROM season_records WHERE type=6 ORDER BY season_ppg asc LIMIT 1;")
+			season_lowppg_tuple = cur.fetchall()
+			con.commit()
+			# Most WINS
+			cur.execute("SELECT season_wins, franchise, season FROM season_records WHERE type=7 ORDER BY season_wins desc LIMIT 1;")
+			season_highwins_tuple = cur.fetchall()
+			con.commit()
+			# Fewest WINS
+			cur.execute("SELECT season_wins, franchise, season FROM season_records WHERE type=8 ORDER BY season_wins asc LIMIT 1;")
+			season_lowwins_tuple = cur.fetchall()
+			con.commit()
+			# Highest avg margin of victory
+			cur.execute("SELECT season_margin, franchise, season FROM season_records WHERE type=9 ORDER BY season_margin desc LIMIT 1;")
+			season_highmargin_tuple = cur.fetchall()
+			con.commit()
+
+			con.close()
+			# weekly_points = weekly_highscore_tuple[0][0]
+			# franchise = weekly_highscore_tuple[0][1]
+			# season = weekly_highscore_tuple[0][2]
+			# week = weekly_highscore_tuple[0][3]
+			# opponent = weekly_highscore_tuple[0][4]
+			return(weekly_highscore_tuple, weekly_lowcore_tuple, weekly_blowout_tuple, weekly_closest_tuple, season_highppg_tuple, season_lowppg_tuple, season_highwins_tuple, season_lowwins_tuple, season_highmargin_tuple)
+			# return(weekly_points, franchise, season, week, opponent)
+		else: 
+			return('none',200)
+	else:
+		con.close()
+		sys.stdout.write('entered db access but no relevant command')
+		return('ok',200)
+
 
 def get_scores(franchise_number, message_type, games_over):
 	########## Get from ClearDb
