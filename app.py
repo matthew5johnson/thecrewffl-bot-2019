be0504058975fe402869a18ad65e3c8ed199aa9a
Matthew <mrcwtwj@gmail.com>
2018-11-18 23:47:27 -0600

    indention error corrected

--- app.py
+++ app.py
@@ -1122,7 +1122,7 @@ def get_standings_2():
 	    
 	    con.close()
 	    
-    rankings_headers = [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+	rankings_headers = [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 	live_standings = '*** Live Standings - based on current live scores ***\n'
 	for i in range(11,-1,-1):
 		live_standings = live_standings + '{}. {} {}-{}-{} pts: {}\n'.format(rankings_headers[i], get_franchise_name(standings_tuple[i][0]), standings_tuple[i][1], standings_tuple[i][2], standings_tuple[i][3], standings_tuple[i][4])
