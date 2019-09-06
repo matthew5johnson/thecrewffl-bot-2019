ad78f6f3116d6469e5c111115cd774496b58da0d
matthew <matthew5johnson@protonmail.com>
2019-09-05 20:20:41 -0500

    typo fixed

--- app.py
+++ app.py
@@ -50,14 +50,13 @@ def scrape_scores():
 	matchups = franchises[12::3]
 
 	live_scoreboard = '*** Week 1 Live Scoreboard ***\n'
-	for i in range(0,12):
+	# for i in range(0,12):
 			# live_scoreboard = live_scoreboard + '{} - {} | proj: {}\n{} - {} | proj: {}\n===== ===== =====\n'.format(scores[i], get_franchise_name(matchups[i]), projected[i])
 			# franchise = get_franchise_name(matchups[i])
 			# live_scoreboard = live_scoreboard + franchise
 			# live_scoreboard = live_scoreboard + '{} - {} | proj: {}\n'.format(scores[i], get_franchise_name(matchups[i]), projected[i])
 	# message = "franchises: {}, proj: {}, scores: {}".format(len(matchups), len(projected), len(scores))
 
-	
 	send_message(matchups)
 	return('ok',200)
 
