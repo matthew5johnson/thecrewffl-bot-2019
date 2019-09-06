57f77b14e116d8719be99e23defc23bf1eaa458b
matthew <matthew5johnson@protonmail.com>
2019-09-05 19:27:30 -0500

    scrape added

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from selenium.webdriver.chrome.options import Options
 import selenium.webdriver.chrome.service as service
 import re
 import pymysql
+from time import sleep
 
 app = Flask(__name__)
 
@@ -19,10 +20,37 @@ def webhook():
 	if '@bot' in data['text']:
 		sender = data['user_id']
 		text = data['text']
-		send_message(sender)
+		scrape_scores()
 		return('ok',200)
 
 	else: return('ok',200)
+
+def scrape_scores():
+	chrome_options = Options()
+	chrome_options.add_argument('--disable-gpu')
+	chrome_options.add_argument('--no-sandbox')
+	chrome_options.add_argument('--headless')
+	driver = webdriver.Chrome(executable_path=os.environ['CHROMEDRIVER_PATH'], chrome_options=chrome_options)
+	driver.get(website)
+	sleep(3)
+	html = driver.page_source
+	soup = BeautifulSoup(html, "html5")
+	driver.close()
+
+	final = str(soup)
+
+	all_franchises = ','.join(re.findall(r'(?<=teamId=)[0-9]*', final))
+	franchises = all_franchises.split(",")
+	all_projected = ','.join(re.findall(r'(?<=Proj Total:<span class="statusValue fw-bold">)[^<]*', final))
+	projected = all_projected.split(",")
+	all_scores = ','.join(re.findall(r'(?<=Score h4 clr-gray-01 fw-heavy tar ScoreCell_Score--scoreboard pl2">)[^<]*', final))
+	scores = all_scores.split(",")
+
+	matchups = franchises[12::3]
+
+	send_message(projected[0])
+
+
 
 def send_message(msg):
 	url = 'https://api.groupme.com/v3/bots/post'
