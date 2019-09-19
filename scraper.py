3274a035276a1dd11af3b49f558ee91cc3aa417b
matthew <matthew5johnson@protonmail.com>
2019-09-19 17:08:36 -0500

    final iteration of progressive time dilation in scraper

--- scraper.py
+++ scraper.py
@@ -17,59 +17,55 @@ def scrape_scores(*args):
     con.close()
     week = int(current_week[0][0])
 
-        
-    # delay = 12
-    added_delay = args[0]
-    
+
+    added_delay = args[0]    
     delay = 4 + added_delay
 
-
-    return(delay)
     
-    # website = "https://fantasy.espn.com/football/league/scoreboard?leagueId=133377&matchupPeriodId={}&mSPID={}".format(week, week)
-    # chrome_options = Options()
-    # chrome_options.add_argument('--disable-gpu')
-    # chrome_options.add_argument('--no-sandbox')
-    # chrome_options.add_argument('--headless')
-    # driver = webdriver.Chrome(executable_path=os.environ['CHROMEDRIVER_PATH'], chrome_options=chrome_options)
-    # driver.get(website)
-    # sleep(delay)
-    # html = driver.page_source
-    # soup = BeautifulSoup(html, "lxml")
-    # driver.close()
+    website = "https://fantasy.espn.com/football/league/scoreboard?leagueId=133377&matchupPeriodId={}&mSPID={}".format(week, week)
+    chrome_options = Options()
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless')
+    driver = webdriver.Chrome(executable_path=os.environ['CHROMEDRIVER_PATH'], chrome_options=chrome_options)
+    driver.get(website)
+    sleep(delay)
+    html = driver.page_source
+    soup = BeautifulSoup(html, "lxml")
+    driver.close()
 
-    # final = str(soup)
+    final = str(soup)
 
-    # all_franchises = ','.join(re.findall(r'(?<=teamId=)[0-9]*', final))
-    # franchises = all_franchises.split(",")
-    # all_projected = ','.join(re.findall(r'(?<=Proj Total:<span class="statusValue fw-bold">)[^<]*', final))
-    # projected = all_projected.split(",")
-    # all_scores = ','.join(re.findall(r'(?<=Score h4 clr-gray-01 fw-heavy tar ScoreCell_Score--scoreboard pl2">)[^<]*', final))
-    # scores = all_scores.split(",")
+    all_franchises = ','.join(re.findall(r'(?<=teamId=)[0-9]*', final))
+    franchises = all_franchises.split(",")
+    all_projected = ','.join(re.findall(r'(?<=Proj Total:<span class="statusValue fw-bold">)[^<]*', final))
+    projected = all_projected.split(",")
+    all_scores = ','.join(re.findall(r'(?<=Score h4 clr-gray-01 fw-heavy tar ScoreCell_Score--scoreboard pl2">)[^<]*', final))
+    scores = all_scores.split(",")
 
-    # matchups = franchises[12::3]
+    matchups = franchises[12::3]
 
-    # con = pymysql.connect(host=os.environ['DB_ACCESS_HOST'], user=os.environ['DB_ACCESS_USER'], password=os.environ['DB_ACCESS_PASSWORD'], database=os.environ['DB_ACCESS_DATABASE'])
-    # cur = con.cursor()
-    # cur.execute("DROP TABLE temporary_scraped_matchups;")
-    # con.commit()
-    # cur.execute("CREATE TABLE temporary_scraped_matchups (game INT, franchise INT, points DECIMAL(4,1), projected DECIMAL(4,1), PRIMARY KEY(franchise));")
-    # con.commit()
+    con = pymysql.connect(host=os.environ['DB_ACCESS_HOST'], user=os.environ['DB_ACCESS_USER'], password=os.environ['DB_ACCESS_PASSWORD'], database=os.environ['DB_ACCESS_DATABASE'])
+    cur = con.cursor()
+    cur.execute("DROP TABLE temporary_scraped_matchups;")
+    con.commit()
+    cur.execute("CREATE TABLE temporary_scraped_matchups (game INT, franchise INT, points DECIMAL(4,1), projected DECIMAL(4,1), PRIMARY KEY(franchise));")
+    con.commit()
 
-    # for i in range(0,len(matchups)):
-    #     cur.execute("INSERT INTO temporary_scraped_matchups VALUES(%s, %s, %s, %s);", (i, matchups[i], scores[i], projected[i]))
-    #     con.commit()
+    for i in range(0,len(matchups)):
+        cur.execute("INSERT INTO temporary_scraped_matchups VALUES(%s, %s, %s, %s);", (i, matchups[i], scores[i], projected[i]))
+        con.commit()
 
-    # ## Test to make sure it scraped all matchups. If not, add to the delay time to give it extra scrape time in the next try
-    # cur.execute("SELECT * FROM temporary_scraped_matchups;")
-    # successful_scrape_test = cur.fetchall()
-    # con.commit()
+    ## Test to make sure it scraped all matchups. If not, add to the delay time to give it extra scrape time in the next try
+    cur.execute("SELECT * FROM temporary_scraped_matchups;")
+    successful_scrape_test = cur.fetchall()
+    con.commit()
 
-    # con.close()
+    con.close()
 
 
-    # if len(successful_scrape_test) < 9:
-    #     scrape_scores(4)
-    #     send_message("... loading ...")
-    # else:
-    #     return('ok', 200)
+    if len(successful_scrape_test) < 9:
+        scrape_scores(4)
+        print("!!! needed 4 extra seconds to scrape !!!")
+    else:
+        return('ok', 200)
